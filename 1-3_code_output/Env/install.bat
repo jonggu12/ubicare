b390c9b42f42920540b53349780bb15488ba5cdd
ensrate <ensrate@gmail.com>
2024-02-18 01:49:10 +0900

    .
    .

--- 1-3_code_output/Env/install.bat
+++ 1-3_code_output/Env/install.bat
@@ -1,5 +1,6 @@
 conda create --name envFirst3 python=3.9
 conda activate envFirst3
 curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe -o miniconda.exe
-
+# Mac
+cd ubicare/1-3_code_output/Env
 pip install -r requirements.txt
